f8963d56f1a61bd9f45bc8ddeb65da6e82b0bb7d
Kauako <kauanhenriquelino06@gmail.com>
2024-05-09 11:53:05 -0300

    criando botao esconder/exibir para o paragrafo

---
+++ script.js
@@ -0,0 +1,16 @@
+    $(document).ready(function() {
+        $('#botao1').click(function()
+        {
+            $('p').hide('slow');
+
+        });
+        $('#botao2').click(function()
+        {
+            $('p').show('slow');
+
+        });
+
+
+
+
+    });
